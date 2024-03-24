a50463c08c325639dab626dc0c0e8f12d9858fe7
Matari73 <marianadeoliveira73@hotmail.com>
2024-03-24 18:54:01 -0300

    Modificando as variáveis para inglês

--- script.js
+++ script.js
@@ -1,34 +1,34 @@
 document.querySelector('button').addEventListener('click', function () {
     let select = document.querySelector('select[name="select"]');
-    let opcao = select.options[select.selectedIndex].value
+    let option = select.options[select.selectedIndex].value
     let input = document.querySelector('input').value
 
     try {
-        fetch(`https://swapi.dev/api/${opcao}/?search=${input}`)
+        fetch(`https://swapi.dev/api/${option}/?search=${input}`)
             .then((resp) => resp.json())
             .then((data) => {
                 console.log(data)
-                const lista = document.querySelector('.lista');
-                lista.innerHTML = '';
+                const list = document.querySelector('.lista');
+                list.innerHTML = '';
 
                 if (data.count >= 1) {
                     data.results.forEach((item) => {
 
-                        switch (opcao) {
+                        switch (option) {
                             case "people":
                                 console.log(item)
-                                let propriedadesPeople = ["name", "height", "mass", "hair_color", "skin_color", "eye_color", "gender"]
-                                createDiv(item, propriedadesPeople);
+                                let peopleProperties = ["name", "height", "mass", "hair_color", "skin_color", "eye_color", "gender"]
+                                createDiv(item, peopleProperties);
                                 break;
                             case "planets":
                                 console.log(item)
-                                let propriedadesPlanets = ["name", "rotation_period", "orbital_period", "diameter", "climate", "gravity", "terrain", "surface_water", "population"]
-                                createDiv(item, propriedadesPlanets)
+                                let planetsProperties = ["name", "rotation_period", "orbital_period", "diameter", "climate", "gravity", "terrain", "surface_water", "population"]
+                                createDiv(item, planetsProperties)
                                 break
                             case "starships":
                                 console.log(item)
-                                let propriedadesStarships = ["name", "model", "length", "passengers", "starship_class", "manufacturer", "cargo_capacity", "cost_in_credits"]
-                                createDiv(item, propriedadesStarships)
+                                let starshipProperties = ["name", "model", "length", "passengers", "starship_class", "manufacturer", "cargo_capacity", "cost_in_credits"]
+                                createDiv(item, starshipProperties)
                                 break
                             default:
                                 break;
@@ -36,7 +36,7 @@ document.querySelector('button').addEventListener('click', function () {
                     });
                 }
                 else {
-                    erro("Nenhum item encontrado")
+                    erro("No items found")
                 }
             })
     } catch (error) {
@@ -44,15 +44,15 @@ document.querySelector('button').addEventListener('click', function () {
     }
 })
 
-function createDiv(item, propriedades) {
+function createDiv(item, properties) {
     const div = document.createElement('div');
     div.classList.add('item');  
 
     for (const [key, value] of Object.entries(item)) {
-        if (propriedades.includes(key)) {
-            const propertyDiv = document.createElement('div');
-            propertyDiv.textContent = `${key}: ${value}`;
-            div.appendChild(propertyDiv);
+        if (properties.includes(key)) {
+            const content = document.createElement('div');
+            content.textContent = `${key}: ${value}`;
+            div.appendChild(content);
         }
     }
     document.querySelector('.lista').appendChild(div);
